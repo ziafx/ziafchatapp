049500db1e9ad097f3a692b4dc6fc5d5b51cba66
Faiz Ahmed <ziafxx1@gmail.com>
2023-07-01 23:58:38 +0530

    deleted few files

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,4 +1,3 @@
-  
 import axios from 'axios';
 
 const url = 'http://localhost:8000';
--- client/src/utils/common-utils.js
+++ client/src/utils/common-utils.js
@@ -1,5 +1,3 @@
-
-
 export const downloadMedia = async (e, originalImage) => {
     e.preventDefault();
     try {
